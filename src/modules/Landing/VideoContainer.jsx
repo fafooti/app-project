dda500b50f0290704c55568f4fe2ce8e8175b931
Fateme <fateme.amiirrii79@gmail.com>
2024-03-25 13:34:15 +0330

    feat :video container created

---
+++ src/modules/Landing/VideoContainer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function VideoContainer() {
+  return (
+    <div className="bg-red-300 flex lg:flex-row justify-between flex-col">
+      <div className="bg-black">video</div>
+      <div className="bg-white" >items</div>
+    </div>
+  );
+}
+
+export default VideoContainer;
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import Layout from "@/layouts/Layout";
 import DeatilsContainer from "@/modules/Landing/DeatilsContainer";
 import Hero from "@/modules/Landing/Hero";
 import PopularProducts from "@/modules/Landing/PopularProducts";
+import VideoContainer from "@/modules/Landing/VideoContainer";
 // import DeatilsContainer from "@/modules/Landing/DeatilsContainer";
 export default function Home() {
   return (
@@ -13,6 +14,7 @@ export default function Home() {
         </div>
         <div className="w-11/12 mx-auto ">
           <PopularProducts />
+          <VideoContainer/>
         </div>
       </main>
     </Layout>
