4e047b7d20d8b419436a37fddb234f3c26a0d928
Fateme <fateme.amiirrii79@gmail.com>
2024-04-02 12:51:46 +0330

    fix: mapping has key

--- src/modules/Landing/DeatilsContainer.jsx
+++ src/modules/Landing/DeatilsContainer.jsx
@@ -3,25 +3,25 @@ import DetailsCard from "./DetailsCard";
 const info = [
   {
     icon: "",
-  
+    id:1,
     title: "1000 + product ",
     desc: "We are proud to have sold over 4,000 products to our customers",
   },
   {
     icon: "",
-   
+    id:2,
     title: "3000+ product solo",
     desc: "We are proud to to our customers",
   },
   {
     icon: "",
-   
+    id:3,
     title: "4000+ product  ",
     desc: "We are proud to have sold over 4,000 products to our customers",
   },
   {
     icon: "",
-  
+   id:4,
     title: "2000+ product solo",
     desc: "We are proud to have sold over 4,000 products to our customers",
   },
@@ -30,7 +30,7 @@ function DeatilsContainer() {
   return (
     <div className="grid md:grid-cols-2 xl:grid-cols-4 gap-8 pb-20 md:w-4/5 xl:max-w-7xl max-w-[90%] mx-auto pt-32 ">
       {info.map((item) => (
-        <DetailsCard title={item.title} desc={item.desc} />
+        <DetailsCard key={item.id} title={item.title} desc={item.desc} />
       ))}
     </div>
   );
