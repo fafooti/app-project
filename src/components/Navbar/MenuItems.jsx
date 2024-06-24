b75ffd82276f35d6ae3b9fd753ee88039a8050a6
Fateme <fateme.amiirrii79@gmail.com>
2024-06-24 14:49:12 +0330

    fix :menuitems fixed

--- src/components/Navbar/MenuItems.jsx
+++ src/components/Navbar/MenuItems.jsx
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import React from 'react'
 import Link from "next/link";
 import Image from "next/image";
@@ -26,34 +26,6 @@ function MenuItems() {
   );
 }
 
-export default MenuItems
-=======
-import React from 'react'
-import Link from "next/link";
-import Image from "next/image";
-import store from "/public/icons/store.svg";
-import faq from "/public/icons/faq.svg"
-import status from "/public/icons/status.svg";
 
-function MenuItems() {
-   
-  return (
-    <div className="text-gray-700 my-8 px-6 flex flex-col  justify-center items-start border-t-2 border-secondaryWhite w-screen ">
-      <Link href="/products" className="flex items-center gap-2 mt-12 ">
-        <Image width={12} height={12} src={store} alt="menu" />
-        Store
-      </Link>
-      <Link href="/" className="flex items-center gap-2  mt-5">
-        <Image width={12} height={12} src={status} alt="menu" />
-        Status
-      </Link>
-      <Link href="/" className="flex items-center gap-2  mt-5 mb-10">
-        <Image width={12} height={12} src={faq} alt="menu" />
-        FAQ
-      </Link>
-    </div>
-  );
-}
 
-export default MenuItems
->>>>>>> ae65b761f98a022c7ea7ac0b27ce5166c385ba23
+export default MenuItems;
